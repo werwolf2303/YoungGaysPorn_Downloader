e9f18be18c91d9f1fc975ac83a8818ec94c8480a
Werwolf2303 <gianlucabeil@outlook.de>
2021-01-24 01:44:33 +0100

    init test branch

---
+++ src/main/java/com/down/Errors.java
@@ -0,0 +1,8 @@
+package com.down;
+
+public class Errors {
+    public void x001() {
+        System.err.println("You must be install Wget in Windows before you can use this program");
+    }
+    public void x010() { System.err.println("Error cant download Porn"); }
+}
