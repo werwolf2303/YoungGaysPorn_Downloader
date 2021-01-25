05b72f9037b7a6ffaf588fcdfa7734272ade6619
werwolf2303 <gianlucabeil@outlook.de>
2021-01-25 19:04:35 +0100

    Update 1

--- YGPDownloader/Program.cs
+++ YGPDownloader/Program.cs
@@ -8,20 +8,60 @@ namespace YGPDownloader
 {
     class Program
     {
+        static bool debug = false;
+        static bool final = false;
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter URL:");
-            string userName = Console.ReadLine();
-            download(userName, "index.html");
-            rIndex();
-            rVideo();
+            try
+            {
+                if(args[0].Equals("--debug"))
+                {
+                    debug = true;
+                    Console.WriteLine("Enter URL:");
+                    string userName = Console.ReadLine();
+                    download(userName, "index.html");
+                    rIndex();
+                    rVideo();
+                    clean();
+                }
+                else
+                {
+                    download(args[0], "index.html");
+                    rIndex();
+                    rVideo();
+                    clean();
+                }
+            }catch(IndexOutOfRangeException ex)
+            {
+                debug = true;
+                help();
+            }
         }
-        static void download(String url, String saveAs)
+        static void help()
         {
-            Console.WriteLine("Downloading...");
-            using (WebClient web1 = new WebClient())
-                web1.DownloadFile(url, saveAs);
-            Console.WriteLine("Download Complete");
+            Console.WriteLine("-- YGPDownloader --\n\nYGPDownloader.exe [DownloadURL]\nYGPDownloader.exe [args]\n\nargs:\n\n--debug (Set to DebugMode)\n\n\nWritten by Werwolf2303 (Gianluca.B)\n-- YGPDownloader --");
+        }
+        static void rVideo()
+        {
+            const Int32 BufferSize = 128;
+            using (var fileStream = File.OpenRead("video.html"))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                String line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (line.Contains("video_url: "))
+                    {
+                        String use = line;
+                        String[] json = use.Split(",");
+                        String conv = json[4].Replace("video_url:", "").Replace("'", "");
+                        String[] split = conv.Split("/?br=");
+                        String[] split2 = split[0].Replace("/?", "").Replace(" ", "\n").Split("\n");
+                        final = true;
+                        download(split2[2], "Porn.mp4");
+                    }
+                }
+            }
         }
         static void rIndex()
         {
@@ -41,26 +81,49 @@ namespace YGPDownloader
                 }
             }
         }
-        static void rVideo()
+        static String getPornName()
         {
             const Int32 BufferSize = 128;
-                using (var fileStream = File.OpenRead("video.html"))
-                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            using (var fileStream = File.OpenRead("index.html"))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                String line;
+                while ((line = streamReader.ReadLine()) != null)
                 {
-                    String line;
-                    while ((line = streamReader.ReadLine()) != null)
+                    if (line.Contains("<title"))
                     {
-                    if (line.Contains("video_url: "))
-                    {
-                        String use = line;
-                        String[] json = use.Split(",");
-                        String conv = json[4].Replace("video_url:", "").Replace("'", "");
-                        String[] split = conv.Split("/?br=");
-                        String[] split2 = split[0].Replace("/?", "").Replace(" ", "\n").Split("\n");
-                        download(split2[2], "Porn.mp4");
-                    }
+                        String title = line.Replace("<title>", "").Replace("</title>", "").Replace(" | Young Gays porn", "");
+                        return title;
                     }
                 }
+            }
+            return null;
+        }
+        static void clean()
+        {
+            File.Delete("index.html");
+            File.Delete("video.html");
+        }
+        static void download(String url, String saveAs)
+        {
+            if (final)
+            {
+                using (WebClient web1 = new WebClient())
+                {
+                    Console.WriteLine("Downloading... (Please wait a moment)");
+                    web1.DownloadFile(url, getPornName() + ".mp4");
+                }
+                Console.WriteLine("Download Complete");
+            }
+            else
+            {
+                using (WebClient web1 = new WebClient())
+                {
+                    Console.WriteLine("Downloading... (Please wait a moment)");
+                    web1.DownloadFile(url, saveAs);
+                }
+                Console.WriteLine("Download Complete");
+            }
         }
     }
 }
